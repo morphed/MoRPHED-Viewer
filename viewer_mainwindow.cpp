3f809b800f6fc864fee2ab914e0aceb6915381b6
Konrad Hafen <khafen74@gmail.com>
2015-07-08 14:49:08 -0600

    Working in basic functionality to load xml

--- viewer_mainwindow.cpp
+++ viewer_mainwindow.cpp
@@ -13,7 +13,72 @@ Viewer_MainWindow::~Viewer_MainWindow()
     delete ui;
 }
 
+void Viewer_MainWindow::updateNumEvents(int nEvents)
+{
+    m_nEvents = nEvents;
+    ui->lbl_floods->setText(QString::number(m_nEvents));
+}
+
+int Viewer_MainWindow::loadXML()
+{
+    m_xmlDoc.loadDocument(m_xmlFilename, 2);
+    updateNumEvents(m_xmlDoc.readNodeData("Events").toInt());
+
+    return PROCESS_OK;
+}
+
+void Viewer_MainWindow::readEventData()
+{
+    readEventData(m_nCurrentEvent);
+}
+
+void Viewer_MainWindow::readEventData(int nEvent)
+{
+    QString eventName = "Event" + QString::number(nEvent);
+
+    m_qvEventVols.clear();
+    m_qvTotalVols.clear();
+    m_qvPngPaths.clear();
+
+    m_qvPngPaths.append(m_xmlDoc.readNodeData(eventName, "HillshadePath"));
+    m_qvPngPaths.append(m_xmlDoc.readNodeData(eventName, "WaterDepthPath"));
+    m_qvPngPaths.append(m_xmlDoc.readNodeData(eventName, "DoDCumulativePath"));
+
+    m_qvEventVols.append(m_xmlDoc.readNodeData(eventName, "ExportedSediment", "Event").toDouble());
+    m_qvTotalVols.append(m_xmlDoc.readNodeData(eventName, "ExportedSediment", "Total").toDouble());
+    m_qvEventVols.append(m_xmlDoc.readNodeData(eventName, "ImportedSediment", "Event").toDouble());
+    m_qvTotalVols.append(m_xmlDoc.readNodeData(eventName, "ImportedSediment", "Total").toDouble());
+    m_qvEventVols.append(m_xmlDoc.readNodeData(eventName, "BedErosion", "Event").toDouble());
+    m_qvTotalVols.append(m_xmlDoc.readNodeData(eventName, "BedErosion", "Total").toDouble());
+    m_qvEventVols.append(m_xmlDoc.readNodeData(eventName, "BedDeposition", "Event").toDouble());
+    m_qvTotalVols.append(m_xmlDoc.readNodeData(eventName, "BedDeposition", "Total").toDouble());
+    m_qvEventVols.append(m_xmlDoc.readNodeData(eventName, "BankErosion", "Event").toDouble());
+    m_qvTotalVols.append(m_xmlDoc.readNodeData(eventName, "BankErosion", "Total").toDouble());
+    m_qvEventVols.append(m_xmlDoc.readNodeData(eventName, "BankDeposition", "Event").toDouble());
+    m_qvTotalVols.append(m_xmlDoc.readNodeData(eventName, "BankDeposition", "Total").toDouble());
+}
+
+int Viewer_MainWindow::setXmlFilename(QString filename)
+{
+    m_xmlFilename = filename;
+
+    return PROCESS_OK;
+}
+
 void Viewer_MainWindow::on_actionOpen_triggered()
 {
+    QString filename;
+    filename = QFileDialog::getOpenFileName(this, "Select *.display.morph file to open");
+    QFileInfo fi(filename);
 
+    if (!fi.exists())
+    {
+        QMessageBox::information(this, "Input file error", "Input file does not exist");
+        return;
+    }
+    else
+    {
+        setXmlFilename(filename);
+        loadXML();
+    }
 }
--- viewer_mainwindow.h
+++ viewer_mainwindow.h
@@ -4,7 +4,9 @@
 #include <QMainWindow>
 #include <QtCore>
 #include <QtGui>
+#include <QFileDialog>
 #include "xmlreadwrite.h"
+#include "viewer_exception.h"
 
 namespace Ui {
 class Viewer_MainWindow;
@@ -18,18 +20,25 @@ public:
     explicit Viewer_MainWindow(QWidget *parent = 0);
     ~Viewer_MainWindow();
 
+    void updateNumEvents(int nEvents);
     int loadXML();
+    void readEventData();
+    void readEventData(int nEvent);
     int setXmlFilename(QString filename);
 
 private slots:
     void on_actionOpen_triggered();
+
+signals:
 
 private:
     Ui::Viewer_MainWindow *ui;
 
     QString m_xmlFilename;
     XMLReadWrite m_xmlDoc;
-    int m_nFloods, m_nCurrentFlood;
+    int m_nEvents, m_nCurrentEvent;
+    QVector<double> m_qvEventVols, m_qvTotalVols;
+    QVector<QString> m_qvPngPaths;
 };
 
 #endif // VIEWER_MAINWINDOW_H
